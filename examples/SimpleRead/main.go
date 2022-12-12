6de19608d503f46b7d0bdb901fa0cd1852a48171
ddeason <dan.deason@logan-aluminum.com>
2022-12-12 13:12:54 -0600

    commented test program

--- examples/SimpleRead/main.go
+++ examples/SimpleRead/main.go
@@ -5,21 +5,31 @@ import (
 	"gologix"
 )
 
+// Demo program for readng an INT tag named "TestInt" in the controller.
 func main() {
 
+	// connect to the PLC
 	client := &gologix.Client{IPAddress: "192.168.2.241"}
 	err := client.Connect()
 	if err != nil {
 		fmt.Printf("Error opening client. %v", err)
 		return
 	}
+	// setup a disconnect.  If you don't disconnect you might have trouble reconnecting
 	defer client.Disconnect()
 
+	// define a variable with a type that matches the tag you want to read.  In this case it is an INT so
+	// int16 or uint16 will work.
 	var tag1 int16
+	// call the read function.
+	// note that tag names are case insensitive.
+	// also note that for atomic types and structs you need to use a pointer.
+	// for slices you don't use a pointer.
 	err = client.Read("testint", &tag1)
 	if err != nil {
 		fmt.Printf("error reading testint. %v", err)
 	}
+	// do whatever you want with the value
 	fmt.Printf("tag1 has value %d", tag1)
 
 }
