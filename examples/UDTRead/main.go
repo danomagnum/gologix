8ed5305b01ba2b8198af2c6ce20f416d9b7c78af
ddeason <dan.deason@logan-aluminum.com>
2022-12-12 16:22:16 -0600

    udt read example

---
+++ examples/UDTRead/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"gologix"
+)
+
+// Demo program for readng an INT tag named "TestInt" in the controller.
+func main() {
+	var err error
+
+	// setup the client.  If you need a different path you'll have to set that.
+	client := gologix.NewClient("192.168.2.241")
+
+	// for example, to have a controller on slot 1 instead of 0 you could do this
+	// client.Path, err = gologix.Serialize(gologix.CIPPort{PortNo: 1}, gologix.CIPAddress(1))
+	// or this
+	// client.Path, err = gologix.ParsePath("1,1")
+
+	// connect using parameters in the client struct
+	err = client.Connect()
+	if err != nil {
+		fmt.Printf("Error opening client. %v", err)
+		return
+	}
+	// setup a deffered disconnect.  If you don't disconnect you might have trouble reconnecting because
+	// you won't have sent the close forward open.  You'll have to wait for the CIP connection to time out
+	// if that happens (about a minute)
+	defer client.Disconnect()
+
+	// define a variable with a type that matches the tag you want to read.  In this case it is a UDT so
+	// we define a struct that matches.
+	type myUDT struct {
+		Field1 int32
+		Field2 float32
+	}
+	var tag1 myUDT
+	// call the read function.
+	// note that tag names are case insensitive.
+	// also note that for atomic types and structs you need to use a pointer.
+	// for slices you don't use a pointer.
+	err = client.Read("TestUDT", &tag1)
+	if err != nil {
+		fmt.Printf("error reading testint. %v", err)
+	}
+	// do whatever you want with the value
+	fmt.Printf("tag1 has value %+v", tag1)
+
+}
