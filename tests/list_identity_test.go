2948abe60f1b49e31882e56a5b4d22edf32abf70
danomagnum <danomagnum@gmail.com>
2025-03-18 21:00:56 -0500

    add integration test

---
+++ tests/list_identity_test.go
@@ -0,0 +1,57 @@
+package gologix_tests
+
+import (
+	"testing"
+
+	"github.com/danomagnum/gologix"
+)
+
+func TestListIdentity(t *testing.T) {
+	// This test is a placeholder for the actual implementation
+	tcs := getTestConfig()
+	for _, tc := range tcs.PlcList {
+		t.Run(tc.PlcAddress, func(t *testing.T) {
+			client := gologix.NewClient(tc.PlcAddress)
+			err := client.Connect()
+			if err != nil {
+				t.Error(err)
+				return
+			}
+			defer func() {
+				err := client.Disconnect()
+				if err != nil {
+					t.Errorf("problem disconnecting. %v", err)
+				}
+			}()
+
+			identity, err := client.ListIdentity()
+			if err != nil {
+				t.Error(err)
+				return
+			}
+
+			if identity == nil {
+				t.Error("identity is nil")
+				return
+			}
+
+			if identity.ProductName != tc.ProductName {
+				t.Errorf("ProductName mismatch. Have %s. Want %s.", identity.ProductName, tc.ProductName)
+			}
+
+			version16 := uint16(tc.SoftwareVersionMinor)<<8 | uint16(tc.SoftwareVersionMajor)
+			if identity.Revision != version16 {
+				t.Errorf("Revision mismatch. Have %d. Want %d.", identity.Revision, version16)
+			}
+
+			if identity.SerialNumber != tc.SerialNumber {
+				t.Errorf("SerialNumber mismatch. Have %d. Want %d.", identity.SerialNumber, tc.SerialNumber)
+			}
+
+			if identity.ProductCode != tc.ProductCode {
+				t.Errorf("ProductCode mismatch. Have %d. Want %d.", identity.ProductCode, tc.ProductCode)
+			}
+
+		})
+	}
+}
