b75e205458461f6ccfeb79bf56a7b4b8f22abfd2
danomagnum <danomagnum@gmail.com>
2026-03-20 22:59:32 -0500

    Add Path Builder type to simplify path generation.

--- cip_definitions.go
+++ cip_definitions.go
@@ -6,13 +6,13 @@ import (
 	"io"
 )
 
-type cipAddress byte
+type CIPAddress byte
 
-func (p cipAddress) Bytes() []byte {
+func (p CIPAddress) Bytes() []byte {
 	return []byte{byte(p)}
 }
 
-func (p cipAddress) Len() int {
+func (p CIPAddress) Len() int {
 	return 0
 }
 
